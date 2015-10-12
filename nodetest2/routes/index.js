c4560c423cf76f1068a45c2597960c0b7793aa04
luparral <lparral@dc.uba.ar>
2015-10-12 18:30:34 -0300

    everything up to add user method in global.js

--- nodetest2/routes/index.js
+++ nodetest2/routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
 module.exports = router;
