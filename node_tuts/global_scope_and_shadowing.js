7d8cb1ece44991427fae2c08e14f0aeab00088bb
Lucia Parral <lu.parral@eudaimonia.com.ar>
2015-10-16 18:07:39 -0300

    chains and closures

---
+++ node_tuts/global_scope_and_shadowing.js
@@ -0,0 +1,12 @@
+
+// Starting with your solution from the previous lesson, assign a value to quux
+// inside foo() (don't use var or let). The value should be different to the
+// value assigned when defining quux inside zip().
+
+function foo() {
+	var bar;
+	quux = 10;
+	function zip(){
+		var quux = 15;
+	}
+}
